2bac4281225133ada1d4f44dbf69fff1f7e04973
Hemajnamburu <114128935+Hemajnamburu@users.noreply.github.com>
2024-10-06 17:55:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        // console.log('Received:', message);
+        
+        // Broadcast the message to all connected clients
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message); // Send message directly as a string
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
